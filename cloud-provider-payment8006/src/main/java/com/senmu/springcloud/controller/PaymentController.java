edb087319815225da2060fc66d4a692f3cce3ce6
Mr.senmu <499633518@qq.com>
2023-03-20 17:59:03 +0800

    v1.7

---
+++ cloud-provider-payment8006/src/main/java/com/senmu/springcloud/controller/PaymentController.java
@@ -0,0 +1,29 @@
+package com.senmu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * 业务名称
+ *
+ * @author dsh
+ * @date 2023-03-17 16:16
+ */
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String servletPort;
+
+    @GetMapping("/consul")
+    public String getServletPort(){
+        return servletPort + ":" + UUID.randomUUID();
+    }
+}
